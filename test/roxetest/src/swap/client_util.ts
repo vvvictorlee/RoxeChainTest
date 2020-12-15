1c40bb31d1eb6cb8ab5155d0fe08dde221c5e3b9
vlbos <vlbos2018@gmail.com>
2020-12-15 16:09:08 +0800

    update swap pricing java interface

--- test/roxetest/src/swap/client_util.ts
+++ test/roxetest/src/swap/client_util.ts
@@ -1,14 +1,11 @@
 export default function date(value: number | string): Date {
     return new Date(value);
 }
-const SWAP_CONTRACT = "roxearntoken";
-const ONE_DECIMALS = 9;
-const sym2dec:{[name:string]:any}={"USD":6,"BTC":8,"ROC":4};
-const MAX_SUPPLY="10000000";
 
 export class ClientUtil {
-    static decimals = Math.pow(10, ONE_DECIMALS);
-    static BONE = Math.pow(10, 9);
+    static para:any;
+    static decimals() { return Math.pow(10, ClientUtil.para.ONE_DECIMALS);}
+    static BONE() { return Math.pow(10, ClientUtil.para.ONE_DECIMALS);}
 
     static require_permissions = ({ account, key, actor, parent }: { account: any, key: any, actor: any, parent: any }) => {
         return {
@@ -92,32 +89,13 @@ export class ClientUtil {
     };
 
 
-    static find_from_array(arr: any[]) {
-        let newArr = arr.filter((p) => {
-            return p.name === "United States";
-        });
-
-        return newArr;
-    }
-
-    static repeat(str: any, n: any) {
-        return new Array(n + 1).join(str);
-    }
-
-    static current_time() {
-        return Date.parse(new Date().toString()) / 1000;
-    }
-
-    static to_timestamp(time: any) {
-        return Date.parse(new Date(time).toString()) / 1000;
-    }
 
     static to_wei(value: any) {
         return value * Math.pow(10, 6);
     }
 
     static to_max_supply(sym: any) {
-        return { quantity: ClientUtil.todecimal(MAX_SUPPLY,sym2dec[sym])+sym, contract: SWAP_CONTRACT };
+        return { quantity: ClientUtil.todecimal(ClientUtil.para.MAX_SUPPLY,ClientUtil.para.sym2dec[sym])+sym, contract: ClientUtil.para.SWAP_CONTRACT };
     }
 
     static get_core_symbol() {
@@ -125,30 +103,30 @@ export class ClientUtil {
     }
 
     static to_sym(sym: any) {
-        return { symbol: ONE_DECIMALS + "," + sym, contract: SWAP_CONTRACT };
+        return { symbol: ClientUtil.para.ONE_DECIMALS + "," + sym, contract: ClientUtil.para.SWAP_CONTRACT };
     }
-    static tounit(value: any,decimals:Number) {
-        return ClientUtil.todecimal(ClientUtil.scalar_decimals(value,decimals),decimals);
-    }
-
-    static todecimal(value: any, decimals: any = ONE_DECIMALS) {
-        return Number(Number(value) / Number(Math.pow(10, decimals))).toFixed(decimals) + " ";
+    static tounit(value: any,decimal:Number) {
+        return ClientUtil.todecimal(ClientUtil.scalar_decimals(value,decimal),decimal);
     }
 
-    static scalar_decimals(value: any,decimals:any) {
-        return Number(value) * Number(Math.pow(10, decimals));
+    static todecimal(value: any, decimal: any = ClientUtil.para.ONE_DECIMALS) {
+        return Number(Number(value) / Number(Math.pow(10, decimal))).toFixed(decimal) + " ";
+    }
+
+    static scalar_decimals(value: any,decimal:any) {
+        return Number(value) * Number(Math.pow(10, decimal));
     }
 
     static to_core_asset(value: any, sym: any) {
-        return { quantity: ClientUtil.tounit(value,sym2dec[sym]) + sym, contract: "roxe.token" };
+        return { quantity: ClientUtil.tounit(value,ClientUtil.para.sym2dec[sym]) + sym, contract: "roxe.token" };
     }
 
     static to_asset(value: any, sym: any) {
-        return { quantity: ClientUtil.todecimal(value,sym2dec[sym]) + sym, contract: SWAP_CONTRACT };
+        return { quantity: ClientUtil.todecimal(value,ClientUtil.para.sym2dec[sym]) + sym, contract: ClientUtil.para.SWAP_CONTRACT };
     }
 
     static to_wei_asset(value: any, sym: any) {
-        return ClientUtil.to_asset(ClientUtil.scalar_decimals(value,sym2dec[sym]), sym);
-        // return ClientUtil.to_asset(ClientUtil.scalar_decimals(Number(value) * (Number(ClientUtil.BONE) / Number(ClientUtil.decimals))), sym);
+        return ClientUtil.to_asset(ClientUtil.scalar_decimals(value,ClientUtil.para.sym2dec[sym]), sym);
+        // return ClientUtil.to_asset(ClientUtil.scalar_decimals(Number(value) * (Number(ClientUtil.BONE()) / Number(ClientUtil.decimals()))), sym);
     }
 }
